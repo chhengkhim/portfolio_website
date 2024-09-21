3e8222259bea8e650b8bdee1ae44d6632782efd2
phok pisethsambo <phokpisethsambo@pisethsambos-MacBook-Pro.local>
2024-09-21 21:40:54 +0700

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,11 +17,9 @@ const About = () => {
               <p>Hi. I'm Phok Pisethsambo , nice to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
+              <p>I’m passionate about building excellent software that enhances the lives of those around me.
+                 I specialize in creating solutions for clients ranging from individuals and small businesses to large enterprise corporations.
+                 Imagine having a software expert available at your fingertips—what could we achieve together?</p>  
             </div>
           </div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,9 +14,9 @@ const Home = () => {
           I'm a Frontend Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a frontend developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
+        I’m a Frontend Developer specializing in crafting exceptional digital experiences. 
+        My passion lies in building responsive and engaging web applications that not only look great but also provide seamless user interactions. 
+        I thrive on translating design concepts into functional interfaces, ensuring that every pixel and line of code contributes to a delightful user journey.
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
